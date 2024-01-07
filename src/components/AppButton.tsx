9d1c16ba502d6b234a277f578de91a5cedd1eba5
Austin <austin.mcguinn@gmail.com>
2024-01-07 02:20:27 +0000

    I went out drinking idk what this is

--- src/components/AppButton.tsx
+++ src/components/AppButton.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
-import Icon from 'react-native-vector-icons/Ionicons';
+import Entypo from 'react-native-vector-icons/Entypo';
 
 // Theme
 import tw from '../../tailwind';
 
 interface ButtonProps {
-  icon?: string;
-  bgColor?: string;
+  icon: string;
+  bgColor: string;
   navWhere?: any;
   disabled?: boolean;
 }
 
 const AppButton = (props: ButtonProps) => {
   const navigation = useNavigation();
-  const {navWhere, disabled} = props;
+  const {icon, bgColor, navWhere, disabled} = props;
 
   return (
     <TouchableOpacity
-      style={tw`flex-row items-center`}
+      style={tw`flex-row items-center justify-center w-17 h-17 rounded-2xl ${bgColor} overflow-hidden`}
       disabled={disabled}
       onPress={() => {
         if (navWhere !== undefined && !disabled) {
@@ -27,11 +27,35 @@ const AppButton = (props: ButtonProps) => {
           navigation.navigate(navWhere);
         }
       }}>
-      <Icon
-        name={'checkmark-sharp'}
-        size={20}
-        style={{color: '#ffffff', paddingRight: 7}}
+      <View
+        style={[
+          tw`bg-black/10`,
+          {
+            position: 'absolute',
+            top: 10,
+            left: -7,
+            width: 150, // adjust size as needed
+            height: 150, // adjust size as needed
+            borderRadius: 75, // half of width/height to make it a circle
+            overflow: 'hidden',
+          },
+        ]}
       />
+      <View
+        style={[
+          tw`bg-black/10`,
+          {
+            position: 'absolute',
+            top: 30,
+            left: 10,
+            width: 150, // adjust size as needed
+            height: 150, // adjust size as needed
+            borderRadius: 75, // half of width/height to make it a circle
+            overflow: 'hidden',
+          },
+        ]}
+      />
+      <Entypo name={icon} size={30} style={{color: '#ffffff'}} />
     </TouchableOpacity>
   );
 };
--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -5,29 +5,35 @@ import AppButton from '../../components/AppButton';
 
 const HomeScreen = () => {
   const buttons = [
-    {title: 'Button 1'},
-    {title: 'Button 2'},
-    {title: 'Button 3'},
-    {title: 'Button 4'},
-    {title: 'Button 5'},
-    {title: 'Button 6'},
-    {title: 'Button 7'},
-    {title: 'Button 8'},
-    {title: 'Button 9'},
-    {title: 'Button 10'},
-    {title: 'Button 11'},
-    {title: 'Button 12'},
-    {title: 'Button 13'},
+    {icon: 'info', bgColor: 'bg-blue-500', navWhere: 'Home'},
+    {icon: 'direction', bgColor: 'bg-pink-500', navWhere: 'Home'},
+    {icon: 'mail', bgColor: 'bg-cyan-400', navWhere: 'Home'},
+    {icon: 'briefcase', bgColor: 'bg-yellow-200', navWhere: 'Home'},
+    {icon: 'twitter', bgColor: 'bg-red-400', navWhere: 'Home'},
+    {icon: 'checkmark-sharp', bgColor: 'bg-white', navWhere: 'Home'},
+    {icon: 'checkmark-sharp', bgColor: 'bg-white', navWhere: 'Home'},
+    {icon: 'checkmark-sharp', bgColor: 'bg-white', navWhere: 'Home'},
+    {icon: 'checkmark-sharp', bgColor: 'bg-white', navWhere: 'Home'},
+    {icon: 'checkmark-sharp', bgColor: 'bg-white', navWhere: 'Home'},
+    {icon: 'checkmark-sharp', bgColor: 'bg-white', navWhere: 'Home'},
+    {icon: 'checkmark-sharp', bgColor: 'bg-white', navWhere: 'Home'},
+    {icon: 'checkmark-sharp', bgColor: 'bg-white', navWhere: 'Home'},
+    {icon: 'checkmark-sharp', bgColor: 'bg-white', navWhere: 'Home'},
   ];
 
   const renderButtonRows = () => {
     const numberOfRows = Math.ceil(buttons.length / 4);
     return Array.from({length: numberOfRows}).map((_, rowIndex) => (
-      <View key={rowIndex} style={tw`flex-row justify-between mb-4`}>
+      <View key={rowIndex} style={tw`flex-row justify-between gap-x-2 mb-4`}>
         {buttons
           .slice(rowIndex * 4, (rowIndex + 1) * 4)
           .map((button, index) => (
-            <AppButton key={index} />
+            <AppButton
+              key={index}
+              icon={button.icon}
+              bgColor={button.bgColor}
+              navWhere={button.navWhere}
+            />
           ))}
       </View>
     ));
@@ -39,7 +45,7 @@ const HomeScreen = () => {
       resizeMode="cover"
       style={tw`flex-1`}>
       <View style={tw`flex-1 bg-black/20`}>
-        <SafeAreaView style={tw`flex-1 m-10`}>
+        <SafeAreaView style={tw`flex-1 m-7`}>
           {/* Time and date */}
           <View style={tw`flex-3 items-center justify-center`}>
             <Text style={tw`text-white text-6xl font-medium`}>19:06</Text>
